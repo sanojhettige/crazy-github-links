1801d4f376fbb608c114cbde8952d948ccd2cec2
sanojhettige <sanojhettige@gmail.com>
2021-09-08 14:32:06 +0530

    initial commit

---
+++ pages/repo/[repoId].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Repo: NextPage = () => {
+  return (
+    <div>
+      Repo by Id
+    </div>
+  )
+}
+
+export default Repo
---
+++ pages/repo/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const LatestRepos: NextPage = () => {
+  return (
+    <div>
+      show recently created repos
+    </div>
+  )
+}
+
+export default LatestRepos
