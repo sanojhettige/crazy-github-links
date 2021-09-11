f79c90540b35fcecab3d321fbccac71ee7e68a74
sanojhettige <sanojhettige@gmail.com>
2021-09-11 16:47:52 +0530

    route link updated

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement, useState } from 'react'
+import Link from 'next/link'
 import Iconography from '../uielements/Iconography';
 
 interface Props {}
@@ -11,12 +12,13 @@ function Navbar(props: Props): ReactElement {
       >
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link href="/">
             <a
               className="text-white text-3xl  title-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-              href="/"
             >
               Github Crazy Links
             </a>
+            </Link>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -34,14 +36,18 @@ function Navbar(props: Props): ReactElement {
             
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
             <li className="flex items-center">
-                <a className="lg:text-white lg:hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold" href="/">
+                <Link href="/">
+                <a className="lg:text-white lg:hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">
                   Home
                 </a>
+                </Link>
               </li>
               <li className="flex items-center">
-                <a className="lg:text-white lg:hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold" href="/about">
+                <Link href="/about">
+                <a className="lg:text-white lg:hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">
                   About
                 </a>
+                </Link>
               </li>
             </ul>
           </div>
