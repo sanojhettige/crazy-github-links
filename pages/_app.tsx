a482b84f7d8c3a961d09265160a5b62ae25a5019
sanojhettige <sanojhettige@gmail.com>
2021-09-11 20:12:36 +0530

    api fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Amplify from 'aws-amplify';
 import 'tailwindcss/tailwind.css';
 import "../public/tailwind.css";
 import 'react-toastify/dist/ReactToastify.css';
-import awsConfig from '../aws-exports';
+import awsConfig from '../src/aws-exports';
 
 Amplify.configure(awsConfig);
 
